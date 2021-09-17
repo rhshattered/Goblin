9c0011cb04eedf8161ec8a0c326249f9efe50705
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-17 10:28:52 -0500

    Changed Order of Ops so project startup doesnt have to be manual

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,53 @@ workspace "Goblin_Engine"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Goblin_Engine/vendor/spdlog/include",
+		"Goblin_Engine/src"
+	}
+
+	links
+	{
+		"Goblin_Engine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GB_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GB_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GB_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GB_DIST"
+		optimize "On"
+
 project "Goblin_Engine"
 	location "Goblin_Engine"
 	kind "SharedLib"
@@ -44,53 +91,6 @@ project "Goblin_Engine"
 		postbuildcommands
 		{
 			"{COPY} %{cfg.buildtarget.relpath}  ../bin/" .. outputdir .. "/Sandbox"
-		}
-
-	filter "configurations:Debug"
-		defines "GB_DEBUG"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "GB_RELEASE"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "GB_DIST"
-		optimize "On"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files 
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"Goblin_Engine/vendor/spdlog/include",
-		"Goblin_Engine/src"
-	}
-
-	links
-	{
-		"Goblin_Engine"
-	}
-
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"GB_PLATFORM_WINDOWS"
 		}
 
 	filter "configurations:Debug"
