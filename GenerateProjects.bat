ba7e80e255ebb65c0f41429657da4c9c500014d2
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-17 10:52:30 -0500

    Changed Color and added runtime selection and made it loop until you exit

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,7 +1,28 @@
 @echo OFF
-color 2
+color d
 title Goblin Project Generator
+:start
+cls
+echo clean             Remove all binaries and generated files
+echo codelite          Generate CodeLite project files
+echo gmake             Generate GNU makefiles for POSIX, MinGW, and Cygwin
+echo gmake2            Generate GNU makefiles for POSIX, MinGW, and Cygwin
+echo vs2005            Generate Visual Studio 2005 project files
+echo vs2008            Generate Visual Studio 2008 project files
+echo vs2010            Generate Visual Studio 2010 project files
+echo vs2012            Generate Visual Studio 2012 project files
+echo vs2013            Generate Visual Studio 2013 project files
+echo vs2015            Generate Visual Studio 2015 project files
+echo vs2017            Generate Visual Studio 2017 project files
+echo vs2019            Generate Visual Studio 2019 project files
+echo xcode4            Generate Apple Xcode 4 project files
+echo Write Editor of choice: 
+set /p input=""
 echo GPG(Goblin Project Generator) Currently only supports vs2019 Automatically
 echo Worse case you open up this batch file and change the premake parameter we cant stop you
-call vendor\bin\premake5\premake5.exe vs2019
+call vendor\bin\premake5\premake5.exe %input%
 PAUSE
+cls
+set /p choice="Do you want to restart? Press 'y' and enter for Yes: "
+if not '%choice%'=='' set choice=%choice:~0,1%
+if '%choice%'=='y' goto start
