1964e8bf888604cb9753c05b4986bd4ebcc74e22
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-18 16:16:29 -0500

    Initial Commit of precompiled header
    Has useful standard library header all in one place

---
+++ Goblin_Engine/src/gbpch.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <stdio.h>
+#include <cmath>
+#include <iostream>
+
+#ifdef GB_PLATFORM_WINDOWS
+#include <Windows.h>
+#include <intrin.h>
+#endif // GB_PLATFORM_WINDOWS
