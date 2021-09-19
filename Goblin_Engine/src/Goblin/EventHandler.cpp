75d89e0274145629aba5c19a76bf2f5276754ae6
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-19 13:13:36 -0500

    Initial Commits
    Beginning of the event system

---
+++ Goblin_Engine/src/Goblin/EventHandler.cpp
@@ -0,0 +1,17 @@
+#include "gbpch.h"
+#include "EventHandler.h"
+struct Event
+{
+	Event();
+
+};
+namespace Goblin {
+	EventHandler::EventHandler()
+	{
+		
+	}
+	void EventHandler::Update()
+	{
+
+	}
+}
---
+++ Goblin_Engine/src/Goblin/EventHandler.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Core.h"
+
+namespace Goblin {
+	class GOBLIN_API EventHandler
+	{
+	public:
+		EventHandler();
+		~EventHandler();
+		void Update();   
+	};
+ 
+}
+
