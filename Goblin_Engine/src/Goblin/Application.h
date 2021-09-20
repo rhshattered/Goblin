bc174f2b8ac5bef556d1e95f61d1242eb7059fef
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-20 11:41:47 -0500

    Added defines for GLFW

--- Goblin_Engine/src/Goblin/Application.h
+++ Goblin_Engine/src/Goblin/Application.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "Core.h"
+#include "Window.h"
 namespace Goblin {
 	class GOBLIN_API Application
 	{
 	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
 		Application();
 		virtual ~Application();
 
 		void Run();
+	private:
+		Window window{WIDTH, HEIGHT, "Goblin Engine"};
+		
 	};
 	// to be defined in client
 	Application* CreateApplication();
