914ee8441b2043777a5ccefec76a445cea0fa667
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-20 11:44:38 -0500

    Created Window definition
    This is the cpp file responsible for the main window

---
+++ Goblin_Engine/src/Goblin/Window.cpp
@@ -0,0 +1,35 @@
+#include "gbpch.h"
+#include "Window.h"
+#include "Log.h"
+namespace Goblin {
+	Window::Window(int w, int h, std::string name) : width{ w }, height{ h }, windowName{ name } 
+	{
+		initWindow();
+	}
+
+	Window::~Window()
+	{
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+
+	void Window::initWindow()
+	{
+		if (!glfwInit())
+		{
+			GB_CORE_ERROR("Failed to initialize glfw library");
+		}
+		//glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+		window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+		if (window == 0)
+		{
+			GB_CORE_ERROR("Failed to create window");
+		}
+		else
+		{
+			GB_CORE_INFO("Window Created");
+		}
+	}
+}
