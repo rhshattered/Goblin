1566a97450f66dc6627bffc72d8bc32fcf5f8ce5
Noah Lynn <noahjacoblynn@gmail.com>
2021-09-23 00:44:15 -0500

    Added new headers

--- Goblin_Engine/src/Goblin.h
+++ Goblin_Engine/src/Goblin.h
@@ -2,7 +2,8 @@
 
 #include "gbpch.h"
 // For use by Goblin Applications
-
+#include "Goblin/Window.h"
+#include "Goblin/Render.h"
 // ----Logging----
 #include "Goblin/Log.h"
 #include "Goblin/ComponentLogger.h"
