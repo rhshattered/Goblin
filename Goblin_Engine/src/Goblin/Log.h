54926ddadb9756a026a163ee16c5dc7ee29b6e54
ttvtempest17 <noahjacoblynn@gmail.com>
2021-09-25 06:29:57 -0500

    Fixed Improper Critical Erorr Definition

--- Goblin_Engine/src/Goblin/Log.h
+++ Goblin_Engine/src/Goblin/Log.h
@@ -22,11 +22,11 @@ namespace Goblin {
 #define GB_CORE_INFO(...)   ::Goblin::Log::GetCoreLogger()->info(__VA_ARGS__)
 #define GB_CORE_WARN(...)   ::Goblin::Log::GetCoreLogger()->warn(__VA_ARGS__)
 #define GB_CORE_ERROR(...)  ::Goblin::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define GB_CORE_FATAL(...)  ::Goblin::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+#define GB_CORE_CRITICAL(...)  ::Goblin::Log::GetCoreLogger()->critical(__VA_ARGS__)
 
 //Client og macros
 #define GB_TRACE(...)       ::Goblin::Log::GetClientLogger()->trace(__VA_ARGS__)
 #define GB_INFO(...)        ::Goblin::Log::GetClientLogger()->info(__VA_ARGS__)
 #define GB_WARN(...)        ::Goblin::Log::GetClientLogger()->warn(__VA_ARGS__)
 #define GB_ERROR(...)       ::Goblin::Log::GetClientLogger()->error(__VA_ARGS__)
-#define GB_FATAL(...)       ::Goblin::Log::GetClientLogger()->fatal(__VA_ARGS__)
+#define GB_CRITICAL(...)       ::Goblin::Log::GetClientLogger()->critical(__VA_ARGS__)
