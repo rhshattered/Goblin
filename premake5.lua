8b97b6d4be4b029ce2cf75c22da466a015055146
Erin <noahjacoblynn@gmail.com>
2021-09-26 16:04:45 -0500

    Changed project structure
    Added GLFW project creation for the submodule

--- premake5.lua
+++ premake5.lua
@@ -44,6 +44,82 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "Goblin_Engine/vendor/glad"
 
+project "glfw"
+	kind "StaticLib"
+	language "C"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"Goblin_Engine/vendor/glfw/GLFW/glfw3.h",
+		"Goblin_Engine/vendor/glfw/include/GLFW/glfw3native.h",
+		"Goblin_Engine/vendor/glfw/src/glfw_config.h",
+		"Goblin_Engine/vendor/glfw/src/context.c",
+		"Goblin_Engine/vendor/glfw/src/init.c",
+		"Goblin_Engine/vendor/glfw/src/input.c",
+		"Goblin_Engine/vendor/glfw/src/monitor.c",
+		"Goblin_Engine/vendor/glfw/src/vulkan.c",
+		"Goblin_Engine/vendor/glfw/src/window.c"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		files
+		{
+			"Goblin_Engine/vendor/glfw/src/win32_init.c",
+			"Goblin_Engine/vendor/glfw/src/win32_joystick.c",
+			"Goblin_Engine/vendor/glfw/src/win32_monitor.c",
+			"Goblin_Engine/vendor/glfw/src/win32_time.c",
+			"Goblin_Engine/vendor/glfw/src/win32_thread.c",
+			"Goblin_Engine/vendor/glfw/src/win32_window.c",
+			"Goblin_Engine/vendor/glfw/src/wgl_context.c",
+			"Goblin_Engine/vendor/glfw/src/egl_context.c",
+			"Goblin_Engine/vendor/glfw/src/osmesa_context.c"
+		}
+
+		defines 
+		{ 
+			"_GLFW_WIN32",
+			"_CRT_SECURE_NO_WARNINGS"
+		}
+
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		
+		files
+		{
+			"Goblin_Engine/vendor/glfw/src/x11_init.c",
+			"Goblin_Engine/vendor/glfw/src/x11_monitor.c",
+			"Goblin_Engine/vendor/glfw/src/x11_window.c",
+			"Goblin_Engine/vendor/glfw/src/xkb_unicode.c",
+			"Goblin_Engine/vendor/glfw/src/posix_time.c",
+			"Goblin_Engine/vendor/glfw/src/posix_thread.c",
+			"Goblin_Engine/vendor/glfw/src/glx_context.c",
+			"Goblin_Engine/vendor/glfw/src/egl_context.c",
+			"Goblin_Engine/vendor/glfw/src/osmesa_context.c",
+			"Goblin_Engine/vendor/glfw/src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
@@ -124,7 +200,7 @@ project "Goblin_Engine"
 	libdirs {"%{prj.name}/vendor/glfw/lib"}
 	-- Our static lib should not link against GLFW
 	links {
-		"opengl32", "glfw3_mt",
+		"opengl32", "glfw",
 		"glad"
 	}
 
